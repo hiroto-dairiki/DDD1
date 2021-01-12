157bc953d4da92fc31bd396dfcba329fe65dabdb
hiroto-dairiki <dairiki.hiroto@itfllc.co.jp>
2021-01-12 15:24:18 +0900

    Initial commit

---
+++ DDD1.WinForm/Common/CommonConst.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD1.WinForm.Common
+{
+    public static class CommonConst
+    {
+        public const string TemperatureUnitName = "℃";
+        public const int TemperatureDecimalPoint = 2;
+
+        public const string ConnectionString = @"Data Source = C:\C#\DDD1.db;Version=3;";
+    }
+}
---
+++ DDD1.WinForm/Data/WeatherSQLite.cs
@@ -0,0 +1,40 @@
+﻿using DDD1.WinForm.Common;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD1.WinForm.Data
+{
+    public static class WeatherSQLite
+    {
+        public static DataTable GetLatest(int areaId)
+        {
+            string sql = @"
+SELECT DataDate,
+       Condition,
+       Temperature
+FROM Weather
+WHERE AreaId = @AreaId
+ORDER BY DataDate desc
+LIMIT 1
+";
+            DataTable dt = new DataTable();
+            using (var connection = new SQLiteConnection(CommonConst.ConnectionString))
+            using (var command = new SQLiteCommand(sql, connection))
+            {
+                connection.Open();
+
+                command.Parameters.AddWithValue("@AreaId", areaId);
+                using (var adapter = new SQLiteDataAdapter(command))
+                {
+                    adapter.Fill(dt);
+                }
+            }
+            return dt;
+        }
+    }
+}

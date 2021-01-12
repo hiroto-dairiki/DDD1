febd72ffb750f7b91a363d8f5b38967eb13d0e1e
hiroto-dairiki <dairiki.hiroto@itfllc.co.jp>
2021-01-12 15:56:43 +0900

    initial commit

---
+++ DDD1Test.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using DDD1.WinForm.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DDD1Test.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var val = Class1.Add(1, 2);
+            Assert.AreEqual(3, val);
+        }
+    }
+}

65a67893a2d37663eae6692b808bae759961517a
hiroto-dairiki <dairiki.hiroto@itfllc.co.jp>
2021-01-12 17:35:44 +0900

    Mocｑでのテスト

--- DDD1.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DDD1.Domain
-{
-    public class Class1
-    {
-    }
-}
---
+++ DDD1.Domain/Repositories/IWeatherRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace DDD1.Domain.Repositories
+{
+    public interface IWeatherRepository
+    {
+        DataTable GetLatest(int areaId);
+    }
+}
